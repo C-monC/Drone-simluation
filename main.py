e024d3d38ca8da58363d043309232abf444d7424
oboontoo <simoncharvey@gmail.com>
2023-10-15 14:04:05 +0200

    fix bug in model simulation

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ import gymnasium as gym
 import os
 from stable_baselines3 import PPO
 
+TRAINING = True
 
 env = QuadrotorPlusHoverEnv()
 
 model = PPO("MlpPolicy", env, verbose=1)
 if os.path.isfile("temp.model"):
     model.load("temp.model")
-# model.learn(total_timesteps=100000)
 
-model.save("temp.model")
+if TRAINING:
+    model.learn(total_timesteps=100000)
+    model.save("temp.model")
 
 num_steps = 1500
 
-obs = env.reset()
+obs, _ = env.reset()
 
 # vec_env = model.get_env()
 for i in range(num_steps):
-    action, _states = model.predict(obs[0], deterministic=True)
+    action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, truncs, info = env.step(action)
     env.render()
     # VecEnv resets automatically
